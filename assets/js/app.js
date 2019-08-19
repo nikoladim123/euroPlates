5e18af199d9d0aa5937e9f3370f9b6f03a811812
unknown <nikoladimitrijevic1991@gmail.com>
2019-08-19 14:49:22 +0200

    euro plates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 var headerPlates = document.getElementsByClassName('headerPlates');
+var orangeOverlay = document.getElementsByClassName('orangeOverlay');
 
 
 var platesModule = 0;
@@ -6,7 +7,20 @@ function platesCar() {
   headerPlates[0].style.opacity = '0';
   headerPlates[1].style.opacity = '0';
   headerPlates[2].style.opacity = '0';
+  orangeOverlay[0].style.width = '33vw';
+  orangeOverlay[0].style.right = '0vw';
   setTimeout(function () {
+    orangeOverlay[0].style.right = '33vw';
+    orangeOverlay[0].style.width = '0vw';
+    setTimeout(function () {
+      orangeOverlay[0].style.transition = 'none';
+      setTimeout(function () {
+        orangeOverlay[0].style.right = '0vw';
+        setTimeout(function () {
+          orangeOverlay[0].style.transition = 'all 0.5s ease';
+        }, 100);
+      }, 100);
+    }, 500);
     headerPlates[platesModule % 3].style.opacity = 1;
     platesModule++;
     console.log(platesModule);
